e7db02ace44b88b8395078985b30ff00a3ad4e5d
stephen ligtenberg <bountyhunter24@gmail.com>
2022-12-05 20:42:45 -0500

    _

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,22 +1,36 @@
 import React, { useState } from "react";
 
 function Form() {
-  const [firstName, setFirstName] = useState("John");
-  const [lastName, setLastName] = useState("Henry");
+  const [formData, setFormData] = useState({
+    firstName: "John",
+    lastName: "Henry",
+  });
 
-  function handleFirstNameChange(event) {
-    setFirstName(event.target.value);
+  function handleChange(event) {
+    // name is the KEY in the formData object we're trying to update
+    const name = event.target.name;
+    const value = event.target.value;
+  
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
   }
-
-  function handleLastNameChange(event) {
-    setLastName(event.target.value);
-  }
-
+  
   return (
     <form>
-      <input type="text" onChange={handleFirstNameChange} value={firstName} />
-      <input type="text" onChange={handleLastNameChange} value={lastName} />
-      <button type="submit">Submit</button>
+      <input
+        type="text"
+        name="firstName"
+        value={formData.firstName}
+        onChange={handleChange}
+      />
+      <input
+        type="text"
+        name="lastName"
+        value={formData.lastName}
+        onChange={handleChange}
+      />
     </form>
   );
 }
